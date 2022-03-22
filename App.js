10bb25b3a138714a45a9ae452f3b743e1c78cd54
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-03-22 22:30:49 +0100

    Improwing the layout

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your Goal!" />
         <Button title="Add Goal"></Button>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of Goals...</Text>
       </View>
     </View>
@@ -16,17 +16,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputcontainter: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "75%",
+    height: 40,
     marginRight: 8,
     paddingLeft: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
